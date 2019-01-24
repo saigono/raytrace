b57bdd37998131d3d6404c3cf174ddb082882fb0
Danil Ineev <dineev@tenable.com>
2019-01-24 17:57:18 +0000

    camera can be positioned now

--- src/camera.rs
+++ src/camera.rs
@@ -8,22 +8,27 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(vfov: f32, aspect: f32) -> Self {
+    pub fn new(look_from: Vec3, look_at: Vec3, vup: Vec3, vfov: f32, aspect: f32) -> Self {
         let theta = vfov * std::f32::consts::PI / 180.0;
         let half_height = (theta / 2.0).tan();
         let half_width = aspect * half_height;
+
+        let w = Vec3::unit(&(look_from - look_at));
+        let u = Vec3::unit(&Vec3::cross(&vup, &w));
+        let v = Vec3::cross(&w, &u);
+
         Self {
-            lower_left_corner: Vec3(-half_width, -half_height, -1.0),
-            horizontal: Vec3(2.0 * half_width, 0.0, 0.0),
-            vertical: Vec3(0.0, 2.0 * half_height, 0.0),
-            origin: Vec3(0.0, 0.0, 0.0),
+            lower_left_corner: look_from - half_width * u - half_height * v - w,
+            horizontal: 2.0 * half_width * u,
+            vertical: 2.0 * half_height * v,
+            origin: look_from,
         }
     }
 
     pub fn get_ray(&self, u: f32, v: f32) -> Ray {
         Ray::new(
             self.origin,
-            self.lower_left_corner + u * self.horizontal + v * self.vertical,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
         )
     }
 }
--- src/linalg/vector.rs
+++ src/linalg/vector.rs
@@ -21,6 +21,14 @@ impl Vec3 {
     pub fn dot(a: &Vec3, b: &Vec3) -> f32 {
         a.0 * b.0 + a.1 * b.1 + a.2 * b.2
     }
+
+    pub fn cross(a: &Vec3, b: &Vec3) -> Vec3 {
+        Vec3::new(
+            a.1 * b.2 - a.2 * b.1,
+            -(a.0 * b.2 - a.2 * b.0),
+            a.0 * b.1 - a.1 * b.0,
+        )
+    }
 }
 
 impl std::ops::Add for Vec3 {
